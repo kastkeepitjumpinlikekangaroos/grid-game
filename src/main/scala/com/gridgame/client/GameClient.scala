d49ddb65337194100d8a16ef91fc2bcd41193c78
Owen Christie <owenchristie@Owens-iMac.local>
2026-02-14 14:40:58 -0500

    fire indicator

--- src/main/scala/com/gridgame/client/GameClient.scala
+++ src/main/scala/com/gridgame/client/GameClient.scala
@@ -1076,6 +1076,9 @@ class GameClient(serverHost: String, serverPort: Int, initialWorld: WorldData, v
     mouseWorldY = y
   }
 
+  def getMouseWorldX: Double = mouseWorldX
+  def getMouseWorldY: Double = mouseWorldY
+
   private def teleportToMouse(): Unit = {
     val world = currentWorld.get()
     val oldPos = localPosition.get()
